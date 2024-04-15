a25282e148595f7ff9af3c391e0b8746080cfd46
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2024-04-15 21:38:40 +0200

    Adding solutions

--- src/main/java/ua/skillup/part1/StringValidator.java
+++ src/main/java/ua/skillup/part1/StringValidator.java
@@ -2,26 +2,26 @@ package ua.skillup.part1;
 
 public class StringValidator {
     public static boolean isValidBinaryNumber(String str) {
-        return false;
+        return str.matches("^[01]+$");
     }
 
     public static boolean isValidHexNumber(String str) {
-        return false;
+        return str.matches("^0x[0-9A-Fa-f]+$");
     }
 
     public static boolean isValidUkrainianPhoneNumber(String str) {
-        return false;
+        return str.replace(" ", "").matches("^\\+?(38\\(0\\d{2}\\)|380\\d{2}|0\\d{2})\\d{7}$");
     }
 
     public static boolean isValidIPv4(String str) {
-        return false;
+        return str.matches("^(25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)\\.(25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)$");
     }
 
     public static boolean isValidPassword(String str) {
-        return false;
+        return str.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[~!@#$%^&*()_+\\-=<>?';.,/])(?=\\S+$).{8,}$");
     }
 
     public static boolean isValidHTMLLink(String str) {
-        return false;
+        return str.matches("<a href=\"(http://www\\..+|https://www\\..+|www\\..+|http://.+|https://.+|ftp://.+|mailto:.+|#\\w*|/\\w*)\">.*</a>");
     }
 }
