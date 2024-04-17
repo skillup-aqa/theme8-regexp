5f261cf4eaf13c471ba0eb9f064ab07480aa9dd5
Sasha2511 <nedzelnytskyis@gmail.com>
2024-04-17 22:06:30 +0300

    isValidBinaryNumber and isValidHexNumber resolving

--- src/main/java/ua/skillup/part1/CheckerForValidator.java
+++ src/main/java/ua/skillup/part1/CheckerForValidator.java
@@ -2,6 +2,8 @@ package ua.skillup.part1;
 
 public class CheckerForValidator {
     public static void main(String[] args) {
+        System.out.println("Binary check 0100101: " + StringValidator.isValidBinaryNumber("0100101"));
+        System.out.println("Hex check 0x00101343dfbBA: " + StringValidator.isValidHexNumber("0x00101343dfbBA"));
 
     }
 
--- src/main/java/ua/skillup/part1/StringValidator.java
+++ src/main/java/ua/skillup/part1/StringValidator.java
@@ -2,11 +2,13 @@ package ua.skillup.part1;
 
 public class StringValidator {
     public static boolean isValidBinaryNumber(String str) {
-        return false;
+
+        return str.matches("^[0-1]+$");
     }
 
     public static boolean isValidHexNumber(String str) {
-        return false;
+
+        return str.matches("^0[xX]?[0-9a-fA-F]+$");
     }
 
     public static boolean isValidUkrainianPhoneNumber(String str) {
