f130c1c4114e782b88fbdfb5cd4c8d6504fb9723
Sasha2511 <nedzelnytskyis@gmail.com>
2024-04-21 16:00:18 +0300

    CaesarCipher resolving (spied on Keys)

--- src/main/java/ua/skillup/part2/CaesarCipher.java
+++ src/main/java/ua/skillup/part2/CaesarCipher.java
@@ -3,10 +3,21 @@ package ua.skillup.part2;
 public class CaesarCipher {
 
     public static String encrypt(String text, int shift) {
-        return null;
+        char[] chars = text.toCharArray();
+        char[] result = new char[chars.length];
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isAlphabetic(chars[i])) {
+                char start = Character.isLowerCase(chars[i]) ? 'a' : 'A';
+                result[i] = (char) (start + (chars[i] - start + shift) % 26);
+            } else {
+                result[i] = chars[i];
+            }
+        }
+
+        return new String(result);
     }
 
     public static String decrypt(String text, int shift) {
-        return null;
+        return encrypt(text, 26 - shift);
     }
 }
--- src/main/java/ua/skillup/part2/CheckPart2Theme8.java
+++ src/main/java/ua/skillup/part2/CheckPart2Theme8.java
@@ -8,5 +8,13 @@ public class CheckPart2Theme8 {
         System.out.println("Light Amplification by Stimulated Emission of Radiation: " +
                 AcronymMaker.makeAcronym("Light Amplification by Stimulated Emission of Radiation"));
         System.out.println("This is a sentence with seven words: " +WordCounter.countWords("This is a sentence with seven words."));
+
+        System.out.println(Character.isAlphabetic('s'));
+        System.out.println(Character.isLowerCase('L') ? 'a' : 'A');
+        System.out.println((char) 97);
+
+        System.out.println(CaesarCipher.encrypt("bastion 12", 5));
+        System.out.println(CaesarCipher.decrypt(CaesarCipher.encrypt("bastion 12", 5), 5));
+
     }
 }
