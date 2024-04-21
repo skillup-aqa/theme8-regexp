35730d6149a65b20fb8695ed0e08a2a691f960eb
Sasha2511 <nedzelnytskyis@gmail.com>
2024-04-21 12:41:19 +0300

    AcronymMaker resolving and CheckPart2Theme8 added

---
+++ src/main/java/ua/skillup/part2/CheckPart2Theme8.java
@@ -0,0 +1,11 @@
+package ua.skillup.part2;
+
+import ua.skillup.part1.StringValidator;
+
+public class CheckPart2Theme8 {
+    public static void main(String[] args) {
+        System.out.println("HyperText Transfer Protocol: " + AcronymMaker.makeAcronym("HyperText Transfer Protocol"));
+        System.out.println("Light Amplification by Stimulated Emission of Radiation: " +
+                AcronymMaker.makeAcronym("Light Amplification by Stimulated Emission of Radiation"));
+    }
+}
