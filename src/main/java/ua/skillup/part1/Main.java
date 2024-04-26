4f96ec550f47c5109e201f09535a948fce5f1ab9
Tetiana Yakymiuk <t882005@yahoo.com>
2024-04-25 20:59:23 -0400

    Adding Main class (for print)

---
+++ src/main/java/ua/skillup/part1/Main.java
@@ -0,0 +1,10 @@
+package ua.skillup.part1;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Checking if number is binary 0001101 - " + StringValidator.isValidBinaryNumber("0001101"));
+        System.out.println("Checking hex number 0x9510A01f00a - " + StringValidator.isValidHexNumber("0x9510A01f00a"));
+
+
+    }
+}
