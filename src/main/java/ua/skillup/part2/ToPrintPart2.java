13b85f77afadc112c33ba0d382ef52336bb7a035
Tetiana Yakymiuk <t882005@yahoo.com>
2024-04-28 19:45:01 -0400

    Resolving AcronymMaker

---
+++ src/main/java/ua/skillup/part2/ToPrintPart2.java
@@ -0,0 +1,13 @@
+package ua.skillup.part2;
+
+public class ToPrintPart2 {
+    public static void main(String[] args) {
+        System.out.println("Acronym marker to Internal Revenue Service - " + AcronymMaker.makeAcronym("Internal Revenue Service"));
+
+
+
+
+
+
+    }
+}
