977007468a38079e8426f4d7925dc0f2e5d13782
Tetiana Yakymiuk <t882005@yahoo.com>
2024-04-28 20:20:41 -0400

    Resolving WordCounter

--- src/main/java/ua/skillup/part2/ToPrintPart2.java
+++ src/main/java/ua/skillup/part2/ToPrintPart2.java
@@ -4,7 +4,7 @@ public class ToPrintPart2 {
     public static void main(String[] args) {
         System.out.println("Acronym marker to Internal Revenue Service - " + AcronymMaker.makeAcronym("Internal Revenue Service"));
 
-
+        System.out.println("Counting numbers of words in a sentence - I have a lot to do tomorrow - cleaning, cooking, studying and homework! - " + WordCounter.countWords("I have a lot to do tomorrow - cleaning, cooking, studying and homework!"));
 
 
 
--- src/main/java/ua/skillup/part2/WordCounter.java
+++ src/main/java/ua/skillup/part2/WordCounter.java
@@ -1,7 +1,16 @@
 package ua.skillup.part2;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class WordCounter {
     public static int countWords(String text) {
-        return 0;
+        Pattern countWordsPattern = Pattern.compile("[0-9A-Za-z]+");
+        Matcher matcher = countWordsPattern.matcher(text);
+        int count = 0;
+        while (matcher.find()){
+            count++;
+        }
+        return count;
     }
 }
